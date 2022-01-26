4193b072fdbd1cf21a56fe215e35f53d5f1308b8
qbentil <Bentilshadrack72@gmail.com>
2022-01-26 00:40:50 +0000

    Implemented FlatView

---
+++ components/FlatView.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+import {FlatList} from 'react-native';
+import Row from './Row'
+
+const renderItem = ({item}) => <Row {...item} />
+const FlatView = (props) => {
+  return(
+    <FlatList
+      renderItem = {renderItem}
+      data = {props.contacts}
+     />
+  )
+}
+
+export default FlatView;
