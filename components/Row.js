3c0cd32318bf441db85788b6a873e3f1f0d54a86
qbentil <Bentilshadrack72@gmail.com>
2022-02-03 19:20:36 +0000

    Styling contact rows

--- components/Row.js
+++ components/Row.js
@@ -4,17 +4,31 @@ import {StyleSheet, Text, View} from 'react-native';
 
 const Row = props =>{
     return (
-      <View key = {props.key}>
-        <Text> {props.name}</Text>
-        <Text> {props.phone}</Text>
+      <View key = {props.key} style = {styles.row}>
+        <Text style = {styles.name}> {props.name}</Text>
+        <Text style = {styles.phone}> {props.phone}</Text>
       </View>
     )
 }
 
 const styles = StyleSheet.create({
   row: {
-    padding: 20,
+    padding: 5,
+    // paddingVertical: 1,
+    margin: 5,
+    backgroundColor: "#8A39E1",
+    borderRadius: 5,
+  },
+  name: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  phone: {
+    color: "#ECC488",
+    fontSize: 15,
+    
   }
-})  
+})
 
 export default Row;
