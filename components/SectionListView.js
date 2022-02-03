89a066eaf07207daebf7ca96f815be12006b5abf
qbentil <Bentilshadrack72@gmail.com>
2022-02-03 17:31:31 +0000

    Updated SectionList View to sort bt First Letter

--- components/SectionListView.js
+++ components/SectionListView.js
@@ -1,22 +1,32 @@
-import * as React from 'react';
+import {SectionList, Text} from 'react-native'
 
-import {SectionList, Text} from 'react-native';
-
+import PropTypes from 'prop-types'
+import React from 'react'
 import Row from './Row'
 
-const renderItem = (obj) => <Row {...obj.item} />
-const renderSectionHeader = obj => <Text>{obj.section.title}</Text>
-const SectionListView = (props) => {
-  return(
-    <SectionList
-      renderItem = {renderItem}
-      renderSectionHeader = {this.renderSectionHeader}
-      sections = {[{
-        title: "Contacts",
-        data: props.contacts
-      }]}
-     />
-  )
+const renderItem = ({item}) => <Row {...item} />
+
+const renderSectionHeader = ({section}) => <Text>{section.title}</Text>
+
+const SectionListView = props => {
+  const contactsByLetter = props.contacts.reduce((obj, contact) => {
+    const firstLetter = contact.name[0].toUpperCase()
+    return {
+      ...obj,
+      [firstLetter]: [...(obj[firstLetter] || []), contact],
+    }
+  }, {})
+
+  const sections = Object.keys(contactsByLetter).sort().map(letter => ({
+    data: contactsByLetter[letter],
+    title: letter,
+  }))
+
+  return <SectionList sections={sections} renderItem={renderItem} renderSectionHeader={renderSectionHeader} />
 }
 
-export default SectionListView;
+SectionListView.propTypes = {
+  contacts: PropTypes.array,
+}
+
+export default SectionListView
